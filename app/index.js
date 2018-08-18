950a3a7caca500c27b71c702f1a56958d37bea69
Luke Michals <luke.michals@gmail.com>
2018-08-18 08:22:30 -0700

    initial commit

---
+++ app/index.js
@@ -0,0 +1,25 @@
+"use strict";
+const Generator = require("yeoman-generator");
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+    this.log("Initializing...");
+  }
+  start() {
+    this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter a name for the new component (i.e.: myNewComponent): "
+      }
+    ]).then(answers => {
+      // create destination folder
+      this.destinationRoot(answers.name);
+      this.fs.copyTpl(
+        this.templatePath("index.html"),
+        this.destinationPath(answers.name + ".html"),
+        { message: "Hello world!" }
+      );
+    });
+  }
+};
